14f13decacd2c1793bee0cbbff66849f9acfbfdf
pommedeterresautee <pommedeterresautee@msn.com>
2014-06-09 19:47:35 +0200

    First commit

---
+++ build.sbt
@@ -0,0 +1,85 @@
+import com.typesafe.sbt.SbtScalariform._
+import sbtassembly.Plugin._
+import sbtassembly.Plugin.AssemblyKeys._
+import scala.Some
+import scalariform.formatter.preferences._
+import sbtrelease.ReleasePlugin._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease._
+import ReleaseStateTransformations._
+
+name := "SuperCSVFile"
+
+version := "1.0.7"
+
+scalaVersion := "2.11.1"
+
+resolvers ++= Seq(
+  "Typesafe repository releases" at "http://repo.typesafe.com/typesafe/releases/"
+)
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.3.3",
+  "com.ibm.icu" % "icu4j" % "53.1",
+  "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "2.1.2",
+  "org.slf4j" % "slf4j-simple" % "1.7.7",
+  "org.scalaz" %% "scalaz-core" % "7.0.6",
+  "org.rogach" %% "scallop" % "0.9.5",
+  "com.github.nscala-time" %% "nscala-time" % "1.2.0",
+  "com.chuusai" %% "shapeless" % "2.0.0",
+  "com.typesafe.play" %% "play-iteratees" % "2.3.0",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.3" % "test",
+  "org.scalatest" %% "scalatest" % "2.2.0" % "test",
+  "commons-codec" % "commons-codec" % "1.9" % "test"
+)
+
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-unchecked",
+  "-feature",
+  "-language:reflectiveCalls",
+  "-Ywarn-dead-code",
+  "-Ywarn-inaccessible",
+  "-Ywarn-nullary-unit",
+  "-Ywarn-nullary-override",
+  "-Ywarn-value-discard"
+)
+
+scalariformSettings
+
+ScalariformKeys.preferences := ScalariformKeys.preferences.value
+  .setPreference(DoubleIndentClassDeclaration, true)
+  .setPreference(AlignSingleLineCaseStatements, true)
+  .setPreference(AlignParameters, true)
+  .setPreference(CompactControlReadability, true)
+  .setPreference(IndentLocalDefs, true)
+  .setPreference(PreserveDanglingCloseParenthesis, true)
+  .setPreference(RewriteArrowSymbols, true)
+  .setPreference(AlignSingleLineCaseStatements.MaxArrowIndent, 100)
+
+net.virtualvoid.sbt.graph.Plugin.graphSettings
+
+releaseSettings
+
+useGlobalVersion := false
+
+releaseProcess := Seq[ReleaseStep](
+  checkSnapshotDependencies,
+  inquireVersions,
+  runClean,
+  runTest,
+  setReleaseVersion,
+  commitReleaseVersion,
+  tagRelease,
+  setNextVersion,
+  commitNextVersion,
+  pushChanges
+)
+
+incOptions := incOptions.value.withNameHashing(nameHashing = true)
+
+assemblySettings
+
+jarName in assembly := { s"${name.value}_v${version.value}.jar" }
+
+mainClass in assembly := Some("com.TAJ.SuperCSVFile.Main")
---
+++ src/test/scala/TestTrait.scala
@@ -0,0 +1,70 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014. TAJ - Société d'avocats
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * EXCEPT AS CONTAINED IN THIS NOTICE, THE NAME OF TAJ - Société d'avocats SHALL
+ * NOT BE USED IN ADVERTISING OR OTHERWISE TO PROMOTE THE SALE, USE OR OTHER
+ * DEALINGS IN THIS SOFTWARE WITHOUT PRIOR WRITTEN AUTHORIZATION FROM
+ * TAJ - Société d'avocats.
+ */
+
+import org.scalatest._
+import com.typesafe.scalalogging.slf4j.{ LazyLogging }
+import akka.testkit.{ TestKitBase, ImplicitSender }
+import akka.actor.ActorSystem
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * Offer an atomic counter.
+ */
+private object TestSystemCounter {
+  private val sysId = new AtomicInteger()
+  def nextSysId() = sysId.incrementAndGet()
+}
+
+trait TestCommonTrait extends ShouldMatchers with LazyLogging
+
+trait TestTrait extends WordSpecLike with TestCommonTrait
+
+/**
+ * For each test, an Akka system is generated, therefore there is no chock in Akka names.
+ */
+trait TestTraitAkka extends fixture.WordSpecLike with TestCommonTrait {
+
+  import TestSystemCounter._
+
+  // ScalaTest needs to know what our Fixture parameter
+  // type is
+  type FixtureParam = AkkaFixture
+  // Our basic Fixture. You would derive from this if you
+  // want to specialize it with more fields, methods, etc...
+  class AkkaFixture extends ImplicitSender with TestKitBase {
+    override implicit lazy val system = ActorSystem(s"TestAkkaSystem${nextSysId()}")
+  }
+  // Abstract. Derivations must implement this
+  def createAkkaFixture(): FixtureParam = new AkkaFixture
+  // This is how our tests get run
+  override def withFixture(test: OneArgTest) = {
+    val sys = createAkkaFixture()
+    test(sys)
+  }
+}
